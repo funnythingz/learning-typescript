a821d7ebf049b6a49e08ae7d9faf5c4a7eda65cb
funnythingz <funnythingz69+github@gmail.com>
2021-05-13 12:56:31 +0900

    added FizzBuzz

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-const message: string = 'Hello'
+const FizzBuzz = (n, x, y) => {
+    let result = []
+    if (n % x === 0) {
+        result.push('F')
+    }
+    if (n % y === 0) {
+        result.push('B')
+    }
+    if (result.length > 0) {
+        return result.join('')
+    }
+    return n
+}
 
-console.log(message)
+for(let i = 1, l = 100; i < l; i++) {
+    console.log(FizzBuzz(i, 3, 9))
+}
